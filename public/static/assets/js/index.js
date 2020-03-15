71bc3c6c214eacb8efcf0666fdbb054352efee53
JHR <1873645345@qq.com>
2020-03-15 11:59:21 +0800

    修改了 关键词

--- public/static/assets/js/index.js
+++ public/static/assets/js/index.js
@@ -25,14 +25,26 @@ $(document).ready(function () {
     // $('.top').click(function () {
     //     $(this).addClass('slideOutUp');
     // })
+    for (var i = 0; i < 20; i++) {
+        // console.log(i)
+        $('.bitem .bline').append(`<span></span>`)
+    }
     var b, c, d, e, l1, l2, l3, l4;
     $('#fullpage').fullpage({
         anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7'],
         menu: '#menu',
         onLeave: function (origin, destination, direction) {
-            console.log(direction, 'leave')
+            // console.log(direction, 'leave')
             var dir = direction;
             var index = origin.index;
+            $('.inner').hide();
+            $('.box').hide();
+            $('.iconbox').hide();
+            $('.logowhite').hide();
+            $('.div').hide();
+            $('.bbox').hide();
+            $('.hbox').hide();
+            $('.bmenu').hide();
             clearTimeout(l1);
             clearTimeout(l2);
             clearTimeout(l3);
@@ -42,16 +54,20 @@ $(document).ready(function () {
             // r('.bg1text', 'slideInUp');
             // r('.bg1en', 'slideInUp');
             // r('.title ','animated slideInDown');
+            r('.t1', 'fadeIn');
+            r('.t2', 'fadeIn');
+            r('.bg1text', 'fadeIn');
             r('.title .en', 'slideInDown');
             r('.title .isword', 'bounceInDown fast');
             r('.title .us', 'animated  zoomIn ');
             r('.face', 'fadeIn');
-            r('.item1', 'flipInY');
-            r('.itemtext', 'slideInUp');
-            r('.item .en', 'slideInUp');
+            r('.item', 'fadeIn slower');
+            r('.itemtext', 'fadeIn slower');
+            r('.item .en', 'fadeIn slower');
             // r('.offerbg', 'slideInUp');
-            r('.iconitem', 'fadeIn');
-            r('.imgs', 'zoomIn');
+            r('.iconitem', 'fadeIn slower');
+            r('.imgs', 'flipInY delay-1s');
+            r('.img .success', 'zoomIn delay-1s');
             r('.line', 'zoomIn');
             r('.l', 'loaded');
             r('.b1 .imgbox', 'imgbox1');
@@ -60,12 +76,54 @@ $(document).ready(function () {
             r('.b4 .imgbox', 'imgbox4');
             r('.qplus', 'swing');
             // r('.hbg', 'slideInDown');
-            r('.midimg', 'zoomIn')
+            // r('.midimg', 'zoomIn')
             r('.imgtext', 'slideInUp');
-            r('.textspan', 'animated slideInDown delay-2s fast');
+            r('.imgbox img', 'bounceIn');
+
+            r('.textspan', 'animated slideInDown delay-1s fast');
             // $('.bitem').animate({'left': '-260px'});
             r('.address', 'flipInX');
             r('.bmenu li', 'flipInY');
+            r('.div>div', 'animated fadeIn slower');
+            r('.lineimg', 'fadeIn slow');
+            r('.midimg', 'zoomIn')
+            r('.logowhite', 'fadeIn')            // l2 = setTimeout(a('.l2', 'loaded'), 1700);
+
+            // r('.bitem.b1 .bline', 'line1');
+            // r('.bitem.b2 .bline', 'line2');
+            // r('.bitem.b3 .bline', 'line3');
+            // r('.bitem.b4 .bline', 'line4');
+            $('.bitem.b1  .bline span').each(function (e) {
+                $(this).animate({
+                    top: -.5 + 'rem',
+                    opacity: 0,
+                    left: -.05 + 'rem'
+                }, 1000);
+            });
+            $('.bitem.b2  .bline span').each(function (e) {
+                $(this).animate(
+                    {
+                        top: 2 + 'rem',
+                        opacity: 0,
+                        right: -3 + 'rem'
+                    },
+                    1000);
+            });
+            $('.bitem.b3  .bline span').each(function (e) {
+                $(this).animate({
+                    bottom: -.8 + 'rem',
+                    opacity: 0,
+                    right: -2 + 'rem'
+                }, 1000);
+            });
+            $('.bitem.b4  .bline span').each(function (e) {
+                $(this).animate(
+                    {
+                        bottom: -2.1 + 'rem',
+                        opacity: 0,
+                        left: -5 + 'rem'
+                    }, 1000);
+            });
             if (dir == 'down') {
                 r('.top', 'slideInDown');
                 a('.top', 'slideOutUp');
@@ -79,41 +137,93 @@ $(document).ready(function () {
 
         },
         afterLoad: function (origin, destination, direction) {
-            // console.log(origin, 'afterLoad',direction)
+            console.log(origin, 'afterLoad', direction)
             var index = origin.index;
+            $('.inner').show();
+            $('.box').show();
+            $('.iconbox').show();
+            $('.logowhite').show();
+            $('.div').show();
+            $('.bbox').show();
+            $('.hbox').show();
+            $('.bmenu').show();
+
             // typing('WE ARE BORN', 't1');
             // a('.bg1text', 'slideInUp');
             // a('.bg1en', 'slideInUp');
             // a('.title ','animated slideInDown');
-            a('.t1', 'bounceInLeft');
-            a('.t2', 'bounceInLeft');
-            a('.bg1text', 'bounceInLeft');
-            a('.title .en', 'slideInDown');
+            a('.t1', 'fadeIn');
+            a('.t2', 'fadeIn');
+            a('.bg1text', 'fadeIn');
+            // a('.title .en', 'slideInDown');
             // a('.title .isword','zoomIn fast');
             // a('.title .us','animated bounceInLeft');
-            a('.title .isword', 'bounceInDown fast');
-            a('.title .us', 'animated  zoomIn ');
+            // a('.title .isword', 'bounceInDown fast');
+            // a('.title .us', 'animated  zoomIn ');
             a('.face', 'fadeIn');
-            a('.item1', 'flipInY');
-            a('.itemtext', 'slideInUp');
-            a('.item .en', 'slideInUp');
-            a('.iconitem', 'fadeIn');
-            a('.imgs', 'zoomIn');
+            a('.item', 'fadeIn slower');
+            // a('.itemtext', 'slideInUp');
+            // a('.item .en', 'slideInUp');
+            a('.iconitem', 'fadeIn slower');
+            a('.imgs', 'fadeIn delay-1s');
+            a('.img .success', 'fadeIn delay-1s');
             a('.line', 'zoomIn');
+
+            // var href = window.location.href;
+            // a('.bitem.b1 .bline', 'line1');
+            // a('.bitem.b2 .bline', 'line2');
+            // a('.bitem.b3 .bline', 'line3');
+            // a('.bitem.b4 .bline', 'line4');
+            $('.bitem.b1  .bline span').each(function (e) {
+                $(this).animate({
+                    top: -.5 + e * .01 + 'rem',
+                    opacity: (20 - e) / 20,
+                    left: -.05 + .2 * e + 'rem'
+                }, 1000);
+            });
+            $('.bitem.b2  .bline span').each(function (e) {
+                $(this).animate(
+                    {
+                        top: 2 + e * .03 + 'rem',
+                        opacity: e / 20,
+                        right: -3 + .2 * e + 'rem'
+                    },
+                    1000);
+            });
+            $('.bitem.b3  .bline span').each(function (e) {
+                $(this).animate({
+                    bottom: -.8 + e * .05 + 'rem',
+                    opacity: (20 - e) / 20,
+                    right: -2 + .3 * e + 'rem'
+                }, 1000);
+            });
+            $('.bitem.b4  .bline span').each(function (e) {
+                $(this).animate(
+                    {
+                        bottom: -2.1 + e * .1 + 'rem',
+                        opacity: e / 20,
+                        left: -5 + .3 * e + 'rem'
+                    }, 1000);
+            });
+
             // a('.bitem', 'slideInLeft');
             // a('.bbox','slideInLeft')
+            a('.imgbox img', 'bounceIn');
+            // a('.imgbox', 'fadeIn');
             a('.b1 .imgbox', 'imgbox1');
             a('.b2 .imgbox', 'imgbox2');
             a('.b3 .imgbox', 'imgbox3');
             a('.b4 .imgbox', 'imgbox4');
-            a('.textspan', 'animated slideInDown delay-2s fast');
+            a('.textspan', 'animated slideInDown delay-1s fast');
+            // $('.bitem').addClass('slideInLeft')
             // $('.bitem').addClass('slideInLeft')
             a('.qplus', 'swing');
             // a('.hbg', 'slideInDown');
-            a('.midimg', 'zoomIn');
+            // a('.midimg', 'zoomIn');
             a('.imgtext', 'slideInUp');
             // a('.l1', 'loaded'),
             a('.hitem', 'animated fadeIn slower');
+
             l1 = setTimeout(function () {
                 $('.l1').addClass('loaded');
                 $('.ll1 .htext').addClass('hovertext')
@@ -140,7 +250,9 @@ $(document).ready(function () {
             }, 2000);
             a('.address', 'flipInX');
             a('.bmenu li', 'flipInY')
-            // l2 = setTimeout(a('.l2', 'loaded'), 1700);
+            a('.div>div', 'animated fadeIn slower');
+            a('.lineimg', 'fadeIn slow');
+            a('.logowhite', 'fadeIn')            // l2 = setTimeout(a('.l2', 'loaded'), 1700);
             // l3 = setTimeout(a('.l3', 'loaded'), 1900);
             // l4 = setTimeout(a('.l4', 'loaded'), 11100);
             // $('.bitem').each(function (index) {
--- public/static/assets/js/mindex.js
+++ public/static/assets/js/mindex.js
@@ -10,33 +10,40 @@ $(document).ready(function () {
     // $('.top').click(function () {
     //     $(this).addClass('slideOutUp');
     // })
-    $('.ricon').click(function () {
+    $('.ricon').click(function (e) {
+        e.stopPropagation()
         $('#menu').slideToggle().toggleClass('a');
         if ($('#menu').hasClass('a')) {
-            $('#ricon').attr('src', './assets/mimgs/close.png')
+            $('#ricon').attr('src', '/static/assets/mimgs/close.png')
         } else {
-            $('#ricon').attr('src', './assets/mimgs/menu.png')
+            $('#ricon').attr('src', '/static/assets/mimgs/menu.png')
         }
     })
-    $('#menu').click(function () {
+    $('#menu').click(function (e) {
+        e.stopPropagation()
         $(this).slideToggle();
         $('#menu').slideToggle().toggleClass('a');
         if ($('#menu').hasClass('a')) {
-            $('#ricon').attr('src', './assets/mimgs/close.png')
+            $('#ricon').attr('src', '/static/assets/mimgs/close.png')
         } else {
-            $('#ricon').attr('src', './assets/mimgs/menu.png')
+            $('#ricon').attr('src', '/static/assets/mimgs/menu.png')
         }
     });
-    $('.fp-section').click(function () {
-        if ($('.top').hasClass('slideInDown')) {
-            r('.top', 'slideInDown');
-            a('.top', 'slideOutUp');
-            $('.top').css('position', 'fixed');
-        } else {
-            a('.top', 'slideInDown');
-            r('.top', 'slideOutUp');
-            $('.top').css('position', 'sticky');
-        }
+    // $('.fp-section').click(function () {
+    //     if ($('.top').hasClass('slideInDown')) {
+    //         r('.top', 'slideInDown');
+    //         a('.top', 'slideOutUp');
+    //         $('.top').css('position', 'fixed');
+    //     } else {
+    //         a('.top', 'slideInDown');
+    //         r('.top', 'slideOutUp');
+    //         $('.top').css('position', 'sticky');
+    //     }
+    // });
+    $('.top').click(function () {
+        r('.top', 'slideInDown');
+        a('.top', 'slideOutUp');
+        $('.top').css('position', 'fixed');
     });
     var mySwiper = new Swiper('.swiper-container', {
         loop: true,
@@ -46,6 +53,7 @@ $(document).ready(function () {
         spaceBetween: 20,
         speed: 1000,
     });
+    var l1, l2, l3, l4;
     $('#fullpage').fullpage({
         anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7'],
         menu: '#menu',
@@ -54,29 +62,45 @@ $(document).ready(function () {
             var d = direction;
             var index = origin.index;
             var anchor = destination.anchor;
-            $('#menu').slideUp().removeClass('a');
-            $('#ricon').attr('src', './assets/mimgs/menu.png')
+            $('.block').hide();
+            $('.swiper-container').hide();
+            $('.bg3linebox').hide();
+            $('.bg4box').hide();
+            $('.bgmid').hide();
+            $('.hbox').hide();
 
-            r('.bg1text', 'slideInDown');
-            r('.bg1en', 'flipInX');
+            $('#menu').slideUp().removeClass('a');
+            $('#ricon').attr('src', '/static/assets/mimgs/menu.png')
+            clearTimeout(l1);
+            clearTimeout(l2);
+            clearTimeout(l3);
+            clearTimeout(l4);
+            r('.l', 'loaded');
+            r('.bg1text', 'fadeIn');
+            r('.bg1en', 'fadeIn');
             r('.face', 'fadeIn');
             r('.item1', 'flipInY');
             r('.itemtext', 'slideInUp');
             r('.item .en', 'slideInUp');
             r('.offerbg', 'zoomIn');
+            r('.imgitem', 'fadeIn slow delay-02s');
+            r('.hbg', 'fadeIn slower delay-04s');
+            r('.midimg', 'fadeIn');
             r('.logowhite', 'slideInUp');
             r('.iconitem', 'zoomIn');
-            r('.bitem', 'slideInDown');
-            r('.imglogo', 'slideInDown');
-            r('.hbg', 'slideInDown');
+            // r('.bitem', 'slideInDown');
+            r('.imglogo', 'fadeIn');
+            // r('.hbg', 'slideInDown');
             r('.textspan', 'bounceIn')
-            r('.bbox .b', 'zoomIn');
-            r('.plus', 'zoomIn')
-            r('.title .a', 'slideInDown');
-            r('.title .b', 'zoomIn');
+            r('.bgmid .b', 'fadeIn');
+            r('.plus', 'zoomIn');
+            r('.qplus', 'swing')
+            // r('.title .a', 'slideInDown');
+            // r('.title .b', 'zoomIn');
             r('.hitem', 'zoomIn');
             r('.address', 'slideInLeft')
-
+            r('.bgmidimg', 'fadeIn');
+            r('.line5', 'fadeIn');
             if (d == 'down') {
                 r('.top', 'slideInDown');
                 a('.top', 'slideOutUp');
@@ -101,29 +125,62 @@ $(document).ready(function () {
         afterLoad: function (origin, destination, direction) {
             // console.log(origin, 'afterLoad',direction)
             // var index = origin.index;
-            a('.bg1text', 'flipInX');
-            a('.bg1en', 'slideInDown');
+            $('.block').show();
+            $('.swiper-container').show();
+            $('.bg3linebox').show();
+            $('.bg4box').show();
+            $('.bgmid').show();
+            $('.hbox').show();
+            a('.bg1text', 'fadeIn');
+            a('.bg1en', 'fadeIn');
             a('.face', 'fadeIn');
-            a('.title .a', 'slideInDown');
-            a('.title .b', 'zoomIn');
-
+            // a('.title .a', 'slideInDown');
+            // a('.title .b', 'zoomIn');
+            a('.swiper-container', 'fadeIn')
             // a('.item1', 'flipInY');
             // a('.itemtext', 'slideInUp');
             // a('.item .en', 'slideInUp');
-            a('.offerbg', 'zoomIn');
-            a('.logowhite', 'slideInUp');
-            a('.iconitem', 'zoomIn');
-            a('.bitem', 'slideInDown');
-            a('.imgitem', 'slideInUp');
-            a('.imglogo', 'slideInDown');
+            // a('.offerbg', 'zoomIn');
+            a('.logowhite', 'fadeIn');
+            a('.iconitem', 'fadeIn');
+            a('.bitem', 'fadeIn');
+            a('.imgitem', 'fadeIn slow delay-02s');
+            a('.hbg', 'fadeIn slower delay-04s');
+            a('.midimg', 'fadeIn');
+            a('.imglogo', 'fadeIn');
             a('.plus', 'zoomIn');
-            // a('.qplus', 'swing');
-            a('.hbg', 'slideInDown');
+            a('.qplus', 'swing');
             a('.textspan', 'bounceIn');
-            a('.bbox .b', 'zoomIn');
+            a('.bgmidimg', 'fadeIn');
+            a('.line5', 'fadeIn');
+            a('.bgmid .b', 'fadeIn');
+            a('.wehate', 'fadeIn slower')
+            l1 = setTimeout(function () {
+                $('.l1').addClass('loaded');
+                $('.ll1 .htext').addClass('hovertext')
+            }, 500);
+            l2 = setTimeout(function () {
+                $('.l2').addClass('loaded');
+                $('.ll2 .htext').addClass('hovertext');
+                $('.ll1 .htext').removeClass('hovertext');
+
+            }, 1000);
+            l3 = setTimeout(function () {
+                $('.l3').addClass('loaded')
+                $('.ll3 .htext').addClass('hovertext');
+                $('.ll2 .htext').removeClass('hovertext');
+
+            }, 1500);
+            l4 = setTimeout(function () {
+                $('.l4').addClass('loaded')
+                $('.ll4 .htext').addClass('hovertext')
+                $('.ll3 .htext').removeClass('hovertext');
+                setTimeout(function () {
+                    $('.ll4 .htext').removeClass('hovertext');
+                }, 500)
+            }, 2000);
             a('.hitem', 'zoomIn');
             a('.address', 'slideInLeft')
         },
     });
-
 });
